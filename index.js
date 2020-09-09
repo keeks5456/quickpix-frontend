76f8a001003cf798169a21a6692200a47c4fcf9b
keeks5456 <Akikogreen08@gmail.com>
2020-09-08 18:07:05 -0700

    ditch the modal and start working on fetches

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 
   let profile = document.querySelector('.profile-container')
   let button = document.querySelector('.btn btn-primary')
-  let mainContainer = document.querySelector('.main-container')
-  const profileContainer = document.querySelector('.profile-container')
+  let mainContainer = document.querySelector('.main')
+  const profileContainer = document.querySelector('.profile')
  
   //fetch user api
   const fetchOneUser = () => {
@@ -54,7 +54,7 @@ fetch(`http://localhost:3000/portraits`)
 }
 fetchAllPhotos()
 
-//build phots
+//build photos
 const buildPortrait = (portrait) => {
 
   // console.log(portrait)
@@ -62,23 +62,17 @@ const buildPortrait = (portrait) => {
   div.className = 'card'
   div.dataset.id = portrait.id
   div.innerHTML = `
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="row">
-            <a href="${portrait.attributes.img_url} " data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-4">
-                <img src= ${portrait.attributes.img_url} class="img-fluid">
-            </a>
-          </div>
-          <h5>description: ${portrait.attributes.description}</h5>
+  <div>
+          <img src= ${portrait.attributes.img_url} class="img-fluid">
+          <h5 class='description'>description: ${portrait.attributes.description}</h5>
           <ul>${commentSection(portrait)}</ul>
-          <div class='likes-btn'${portrait.attributes.likes}"likes-section">
-          add likes to this section 
-        </div>
-      </div>
+          <div class=${portrait.attributes.likes}"likes-section">	 
+          <span class="likes">0 likes</span>	 
+          <button class="like-button">♥</button>
+  </div>
   ` 
-  // console.log(portrait.attributes.description)
-  mainContainer.appendChild(div)
 
+  mainContainer.appendChild(div)
 
 
 
@@ -86,11 +80,9 @@ const buildPortrait = (portrait) => {
 
 
 
-
 // need to do the comments here
 function commentSection(portrait){
   const ul = document.getElementsByName('ul')
-  console.log(ul)
   ul.innerHTML = ''
   const newUl = document.createElement('ul')
   newUl.className = 'comments'
@@ -99,8 +91,28 @@ function commentSection(portrait){
     let li = document.createElement('li')
     li.textContent = comment.content
     newUl.appendChild(li)
-   
-
   })
+
+  const likesSection = document.querySelector('.like-button')
+  // likes.appendChild(newUl)
+
+  console.log(likesSection)
+
   // console.log(portrait.attributes.comments)
 }
+
+
+
+
+
+//this is fr the modal, come back to this later
+// <a href="${portrait.attributes.img_url} " data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-4">
+// <img src= ${portrait.attributes.img_url} class="img-fluid">
+// </a>
+// </div>
+// <h5 class='description'>description: ${portrait.attributes.description}</h5>
+// <ul>${commentSection(portrait)}</ul>
+// <div class='likes-btn'${portrait.attributes.likes}"likes-section">
+// add likes to this section 
+// </div>
+// </div>
